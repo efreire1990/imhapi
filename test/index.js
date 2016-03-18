a15e6f2e0cfe03fc962368776cfcb1158c116ff4
Eladio <eladio@caseonit.com>
2016-03-18 11:06:44 +0100

    miTarea5

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@
 const Code = require('code');   // assertion library
 const Lab = require('lab');
 const Hapi = require('hapi');
-const Server = require('../lib/');
+const Server = require('../lib/index.js');
 const Version = require('../lib/version');
 
 const internals = {};
@@ -18,22 +18,22 @@ const expect = Code.expect;
 
 describe('Test de index', () => {
 
-    it('Test básico para verificar la función init()', (done) => {
-
-        Server.init(0, (err, server) => {
-
-            expect(err).to.not.exist();
-            server.stop(done);
-        });
-    });
-
-    it('Inicia el servidor y devuelve un objeto server de Hapi', (done) => {
+    it('Inicia el servidor y devuelve un objeto server', (done) => {
 
         Server.init(0, (err, server) => {
 
             expect(err).to.not.exist();
             expect(server).to.be.instanceof(Hapi.Server);
-            server.stop(done);//parar el test despues de llamar a init
+            server.stop(done);
+        });
+    });
+
+    it('Test que verificar la función init()', (done) => {
+
+        Server.init(0, (err, server) => {
+
+            expect(err).to.not.exist();
+            server.stop(done);
         });
     });
 
@@ -43,28 +43,28 @@ describe('Test de index', () => {
 
             expect(err).to.not.exist();
             expect(server.info.port).to.equal(8000);
-            server.stop(done);//parar el test despues de llamar a init
+            server.stop(done);
         });
     });
 
-    it('Un test que sabe manejar los errores de registro de un plugin', (done) => {
+    it('errores de registro de un plugin', (done) => {
 
         const orig = Version.register;
 
         Version.register = function (server, options, next) {
 
             Version.register = orig;
-            return next(new Error('Fallo en el registro del plugin'));
+            return next(new Error('Fail'));
         };
 
         Version.register.attributes = {
-            name: 'Versión distinta a la que buscamos'
+            name: 'Versión diferente'
         };
 
         Server.init(0, (err, server) => {
 
             expect(err).to.exist();
-            expect(err.message).to.equal('Fallo en el registro del plugin');
+            expect(err.message).to.equal('Fail');
             done();
         });
     });
--- test/version.js
+++ test/version.js
@@ -1,10 +1,11 @@
 'use strict';
 
+
 const Code = require('code');
-const Lab = require('../');
+const Lab = require('lab');
+const Package = require('../package.json');
 const Server = require('../lib/index.js');
 
-const PackageJson = require('../package.json');
 
 const internals = {};
 
@@ -14,17 +15,20 @@ const describe = lab.experiment;
 const it = lab.test;
 const expect = Code.expect;
 
+
 describe('/version', () => {
 
-    it('nombre paquete', (done) => {
+    it('Devuelve la version del json', (done) => {
 
         Server.init(0, (err, server) => {
 
             expect(err).to.not.exist();
+
             server.inject('/version', (res) => {
 
                 expect(res.statusCode).to.be.equal(200);
-                expect(res.result).to.deep.equal({ version : PackageJson.version });
+                expect(res.result).to.deep.equal({ version: Package.version });
+
                 server.stop(done);
             });
         });
